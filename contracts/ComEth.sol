548868861c8229bc9e2b0bbfa49c4bb1dedfe72b
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-08-16 13:34:34 +0200

    enum strucs

--- contracts/ComEth.sol
+++ contracts/ComEth.sol
@@ -1,10 +1,83 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Address.sol";
+
 contract ComEth {
+        using Address for address payable;
+
+    enum Vote {
+        Yes,
+        No
+    }
+
+    enum StatusVote {
+        Inactive,
+        Running,
+        Approved,
+        Rejected
+    }
+
+    struct User {
+        address userAddress;
+        bool isBanned;
+        bool hasVoted;
+        bool hasPaid;
+        bool isActive;
+    }
+
+    struct Proposal {
+        StatusVote statusVote;
+        bytes32 uuid;
+    }
+
     address private _comEthOwner;
+    bool private _isActive;
+    bool private _hasPaid;
+    
+    mapping (address => User) private _users;
+    mapping (uint256 => Proposal) private _proposals;
 
      constructor(address comEthOwner_){
          _comEthOwner = comEthOwner_ ;
      }
+
+    //pot commun
+    receive() external payable {
+    }
+
+    //votes
+    function submitProposal () public {
+
+    }
+
+    function vote () public {
+
+    }
+
+    //paiement
+    function proceedPaiement () internal {
+
+    }
+
+    //gestion des membres/rôles
+    function _toggleIsActive (address userAddress) private returns (bool) {
+        if (_users[userAddress].isActive = false) {
+            _users[userAddress].isActive = true;
+        } else {
+            _users[userAddress].isActive = false;
+        }
+        return _users[userAddress].isActive;
+    }
+
+    //prison
+    function ban () internal {
+
+    }
+
+    function getIsBanned (address userAddress) public returns (bool) {
+        return _users[userAddress].isBanned;
+    }
+    //fermeture de comEth
+     
 }
--- contracts/ComEthFactory.sol
+++ contracts/ComEthFactory.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "./ComEth.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ComEthFactory {
+contract ComEthFactory is Ownable {
     ComEth[] public comEthAddresses;
+    address private _factoryOwner;
     event ComEthCreated(ComEth comEth);
 
-    constructor() {}
+    constructor(address factoryOwner_) {
+        _factoryOwner = factoryOwner_;
+    }
 
     function createComEth(address comEthOwner_) external {
         ComEth comEth = new ComEth(comEthOwner_);
