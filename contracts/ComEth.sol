56117148071a99f7890416b011eab1bf0c267f62
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-16 16:16:38 +0200

    push

--- contracts/ComEth.sol
+++ contracts/ComEth.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Address.sol";
 
 contract ComEth {
-        using Address for address payable;
-        
-//types de votes
+    using Address for address payable;
+
+    //types de votes
     enum YesNo {
         yes,
         no,
@@ -42,41 +42,37 @@ contract ComEth {
     }
 
     struct Proposal {
+        Vote vote;
         StatusVote statusVote;
         bytes32 uuid;
+        uint256 createdAt;
+        string proposition;
     }
 
     address private _comEthOwner;
     bool private _isActive;
     bool private _hasPaid;
-    
-    mapping (address => User) private _users;
-    mapping (uint256 => Proposal) private _proposals;
 
-     constructor(address comEthOwner_){
-         _comEthOwner = comEthOwner_ ;
-     }
+    mapping(address => User) private _users;
+    mapping(uint256 => Proposal) private _proposals;
+
+    constructor(address comEthOwner_) {
+        _comEthOwner = comEthOwner_;
+    }
 
     //pot commun
-    receive() external payable {
-    }
+    receive() external payable {}
 
     //votes
-    function submitProposal () public {
-        
-    }
+    function submitProposal() public {}
 
-    function vote () public {
-
-    }
+    function vote() public {}
 
     //paiement
-    function proceedPaiement () external payable{
-
-    }
+    function proceedPaiement() external payable {}
 
     //gestion des membres/rôles
-    function _toggleIsActive (address userAddress) private returns (bool) {
+    function _toggleIsActive(address userAddress) private returns (bool) {
         if (_users[userAddress].isActive = false) {
             _users[userAddress].isActive = true;
         } else {
@@ -86,14 +82,11 @@ contract ComEth {
     }
 
     //prison
-    function ban () public {
+    function ban() public {}
 
-    }
-
-    function getIsBanned (address userAddress) public view returns (bool) {
+    function getIsBanned(address userAddress) public view returns (bool) {
         return _users[userAddress].isBanned;
     }
 
     //fermeture de comEth
-     
 }
