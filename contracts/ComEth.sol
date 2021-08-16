e85628b8cff781443d4bfad8ffafd0f17ea96171
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-16 11:48:03 +0200

    cometh in progress

--- contracts/ComEth.sol
+++ contracts/ComEth.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract ComEth {
+    address private _comEthOwner;
 
+     constructor(address comEthOwner_){
+         _comEthOwner = comEthOwner_ ;
+     }
 }
--- contracts/ComEthFactory.sol
+++ contracts/ComEthFactory.sol
@@ -1,6 +1,20 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-contract ComEthFactory {
+import "./ComEth.sol";
 
+contract ComEthFactory {
+    ComEth[] public comEthAddresses;
+    event ComEthCreated(ComEth comEth);
+
+    address private _comEthOwner;
+
+    constructor() {}
+
+    function createComEth(address comEthOwner_) external {
+        ComEth comEth = new ComEth(comEthOwner_);
+
+        comEthAddresses.push(comEth);
+        emit ComEthCreated(comEth);
+    }
 }
