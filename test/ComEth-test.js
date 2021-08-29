3b5d0790ee24bf5bcd3fbcddcc567da8d40e6f28
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-29 15:04:38 +0200

    push

--- test/ComEth-test.js
+++ test/ComEth-test.js
@@ -1,22 +1,23 @@
-/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 const { expect } = require('chai');
 
 describe('ComEth', function () {
-
- let ComEthFactory, comEthFactory, ComEth, comEth, dev, alice, bob, eve;
-
+  let ComEthFactory, comEthFactory, ComEth, comEth, dev, alice, bob, eve;
 
   this.beforeEach(async function () {
     [dev, alice, bob, eve] = await ethers.getSigners();
     ComEthFactory = await ethers.getContractFactory('Token');
     comEthFactory = await ComEthFactory.connect(dev).deploy();
-    await token.deployed();
+    await comEthFactory.deployed();
   });
 
-  describe('ComEthFactory Deployement' , function() {
-    it ('should emit event ')
-  })
-
+  describe('ComEthFactory Deployement', function () {
+    it('should return factoryOwner ', function(){
+      
+    });
+  });
 });
