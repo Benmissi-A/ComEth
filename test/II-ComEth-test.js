c4f09d3ed9b580c7dbdce8d87e6dec003c2ec4f3
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-29 19:01:10 +0200

    push

--- test/II-ComEth-test.js
+++ test/II-ComEth-test.js
@@ -29,15 +29,24 @@ describe('ComEth', function () {
     await comEth.deployed();
   });
   describe('addUsers', function () {
-    
     it('should emit UserAdded', async function () {
       await expect(comEth.addUser(bob.address)).to.emit(comEth, 'UserAdded').withArgs(bob.address);
     });
-
+    it('should revert with Cometh: you are not allowed to add users', async function () {
+      await expect(comEth.connect(bob).addUser(bob.address)).to.be.revertedWith(
+        'ComEth: You are not allowed to add users.'
+      );
+    });
+    it('should revert with ComEth: user already exist', async function () {
+     await comEth.addUser(bob.address)
+     await expect(comEth.addUser(bob.address)).to.be.revertedWith('ComEth: user already exist');
+    });
+  });
+  describe('Pay', function () {
     it('has payed', async function () {
-      await comEth.addUser(alice.address)
-      await comEth.pay()
-      expect(await comEth.getInvestmentBalance(alice.address)).to.equal(subscriptionPrice)
+      await comEth.addUser(alice.address);
+      await comEth.pay();
+      expect(await comEth.getInvestmentBalance(alice.address)).to.equal(subscriptionPrice);
     });
   });
   describe('Submit Proposal', function () {
@@ -58,10 +67,24 @@ describe('ComEth', function () {
         .to.emit(comEth, 'ProposalCreated')
         .withArgs(1, 'qui sont les meilleurs');
     });
-    describe('Vote', function () {
-      it('should emit Voted', async function () {
-        //await expect(comEth.addUser(bob.address)).to.emit(comEth, 'UserAdded').withArgs(bob.address);
-      });
+  });
+  describe('Vote', function () {
+    it('should emit Voted', async function () {
+      await comEth.addUser(alice.address);
+      await comEth.pay();
+      await comEth.submitProposal(
+        ['one', 'two', 'Frééé'],
+        'qui sont les meilleurs',
+        900,
+        eve.address,
+        ethers.utils.parseEther('0.01')
+      );
+      await comEth.addUser(bob.address);
+      await comEth.connect(bob).pay();
+
+      await expect(comEth.connect(bob).vote(1, 1))
+        .to.emit(comEth, 'Voted')
+        .withArgs(bob.address, 1, 'qui sont les meilleurs');
     });
   });
 });
