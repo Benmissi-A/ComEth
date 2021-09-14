14e1cfad9537fe64b84451d64f3db18d75adc3f1
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-09-14 18:18:03 +0200

    abi ok

--- scripts/deploy-ComEthFactory.js
+++ scripts/deploy-ComEthFactory.js
@@ -1,37 +1,31 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable no-undef */
+
 const hre = require('hardhat');
 const { deployed } = require('./deployed');
-const NAME = 'ComEthToken';
-const SYMBOL = 'CET';
-const INITIAL_SUPPLY = ethers.utils.parseEther('8000');
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
-  //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
-  // await hre.run('compile');
+  await hre.run('compile');
 
-  // Optionnel car l'account deployer est utilisé par défaut
   const [deployer] = await ethers.getSigners();
   console.log('Deploying contracts with the account:', deployer.address);
 
-  // We get the contract to deploy
-  const ComEthToken = await hre.ethers.getContractFactory('ComEthToken');
-  const comEthToken = await ComEthToken.deploy(NAME, SYMBOL, INITIAL_SUPPLY);
+  // Gets the contract to be deployed
+  const ComEthFactory = await hre.ethers.getContractFactory('ComEthFactory');
+  const comEthFactory = await ComEthFactory.deploy(deployer.address);
 
-  // Attendre que le contrat soit réellement déployé, cad que la transaction de déploiement
-  // soit incluse dans un bloc
-  await comEthToken.deployed();
+  // Awaits the transaction to be completed, meaning definitely added to a bloc
+  await comEthFactory.deployed();
 
   // Create/update deployed.json and print usefull information on the console.
-  await deployed('ComEthToken', hre.network.name, comEthToken.address);
+  await deployed('ComEthFactory', hre.network.name, comEthFactory.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+// Pattern allowing to use async/await everywhere and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
--- test/I-ComEthFactory-test.js
+++ test/I-ComEthFactory-test.js
@@ -4,12 +4,13 @@
 /* eslint-disable no-unused-vars */
 
 const { expect } = require('chai');
-
-// const { getContractAddress } = require('@ethersproject/address');
+// const { ethers } = require('hardhat');
+const { getContractAddress } = require('@ethersproject/address');
 
 describe('ComEthFactory', function () {
   let ComEthFactory, comEthFactory, dev, alice, bob, eve;
 
+  // it deploys first
   this.beforeEach(async function () {
     [dev, alice, bob, eve] = await ethers.getSigners();
     ComEthFactory = await ethers.getContractFactory('ComEthFactory');
@@ -18,17 +19,26 @@ describe('ComEthFactory', function () {
   });
 
   describe('functions', function () {
+    /* getContractAddress(): the address of the contract can only be generated using the address 
+       of the deployer and its nonce.
+       We import @ethersproject/address for this.
+    */
     it('should emit event ComEthCreated', async function () {
-      // const transactionCount = await alice.getTransactionCount();
-      // const futureAddress = getContractAddress({
-      //   from: alice.address,
-      //   nonce: transactionCount,
-      // });
+      /* const transactionCount = await alice.getTransactionCount();
+      const futureAddress = getContractAddress({
+        from: alice.address,
+        nonce: transactionCount,
+      }); */
       const futureAddress = '0xa16E02E87b7454126E5E10d957A927A7F5B5d2be';
       await expect(comEthFactory.connect(alice).createComEth(ethers.utils.parseEther('0.1')))
         .to.emit(comEthFactory, 'ComEthCreated')
         .withArgs(futureAddress);
     });
+    /* it('should return an array of the ComEth addresses created contract sofar', async function () {
+      await comEthFactory.connect(alice).createComEth(ethers.utils.parseEther('0.1'));
+      const comEthAddress = '0xCafac3dD18aC6c6e92c921884f9E4176737C052c';
+      expect(await comEthFactory.getComEths()).to.equal(comEthAddress);
+    }); */
     it('should return factoryOwner ', async function () {
       expect(await comEthFactory.getFactoryOwner()).to.equal(dev.address);
     });
